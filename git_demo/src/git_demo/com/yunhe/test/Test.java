45c969e70fd313b127ad50fc90ba74cc3bdb265d
1341005547 <Administrator@172.16.6.172>
2017-10-19 15:47:12 +0800

    第二次提交

---
+++ git_demo/src/git_demo/com/yunhe/test/Test.java
@@ -0,0 +1,10 @@
+package git_demo.com.yunhe.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("helloworld");
+	}
+
+}
